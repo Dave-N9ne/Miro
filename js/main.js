b715bcfbb0f9b4767c144f73ed76a2ac68922c29
Dave-N9ne <sergeytretiakov2003@gmail.com>
2024-12-12 20:20:31 +0300

    write code for tabs

--- js/main.js
+++ js/main.js
@@ -9,7 +9,66 @@ function addNavMenu () {
         body.classList.toggle('_lock');
     })
 }
+
 function toggleActiveClass (element) {
     element.classList.toggle('_active');
 }
-addNavMenu ();
+
+addNavMenu();
+
+function TabElements(buttons, contentList) {
+    this.buttons = buttons;
+    this.contentList = contentList;
+    this.addTabs = function() {
+        this.buttons.forEach(function(button, index) {
+            button.addEventListener("click", function(event) {
+                const doesContain = event.target
+                                    .classList.contains('tabs__button');
+                const arrayOfTabLists = [
+                    contentList,
+                    buttons
+                ];
+                if (doesContain) {
+                  arrayOfTabLists.forEach(removeInnerElements);
+                }
+                const content = contentList[index];
+                const arrayOfTabElements = [
+                    content,
+                    button
+                ];
+                arrayOfTabElements.forEach(addActiveClass);
+              });
+        })
+    }
+}
+
+function removeInnerElements (array) {
+    array.forEach(removeActiveClass);
+}
+
+function removeActiveClass(element) {
+    element.classList.remove('active');
+}
+
+function addActiveClass(element) {
+    element.classList.add('active');
+}
+
+function linkTabs() {
+    const workstyleButtons = document.querySelectorAll('.workstyle__button');
+    const workstyleContentList = document.querySelectorAll('.workstyle__content');
+    const commandsButtons = document.querySelectorAll('.commands__button');
+    const commandsContentList = document.querySelectorAll('.commands__content');
+
+    const workstyleTabs = new TabElements(workstyleButtons, workstyleContentList);
+    const commandsTabs = new TabElements(commandsButtons, commandsContentList);
+    const arrayOfTabs = [
+        workstyleTabs,
+        commandsTabs
+    ]
+    arrayOfTabs.forEach(function (object) {
+        object.addTabs();
+    })
+}
+
+linkTabs();
