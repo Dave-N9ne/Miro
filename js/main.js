6351ca6e5f64cc8ac2882d50f86650400217fa54
Dave-N9ne <sergeytretiakov2003@gmail.com>
2025-07-17 12:11:16 +0300

    change function TabElements to class Tabs

--- js/main.js
+++ js/main.js
@@ -1,102 +1,107 @@
-function addNavMenu () {
-    const button = document.querySelector('#header-burger');
-    const nav = document.querySelector('#header-nav-list');
-    const body = document.body;
-    
-    button.addEventListener ('click', function (event) {
-        const elementsToActive = [button, nav];
-        elementsToActive.forEach(toggleActiveClass);
-        body.classList.toggle('lock');
-    })
+function addActiveClass (element) {
+  element.classList.add('active');
+}
+
+function removeActiveClass (element) {
+  element.classList.remove('active');
 }
 
 function toggleActiveClass (element) {
-    element.classList.toggle('active');
+  element.classList.toggle('active');
+}
+
+function addNavMenu () {
+  const button = document.querySelector('#header-burger');
+  const nav = document.querySelector('#header-nav-list');
+  const body = document.body;
+    
+  button.addEventListener ('click', function (event) {
+    const elementsToActive = [button, nav];
+    elementsToActive.forEach(toggleActiveClass);
+    body.classList.toggle('lock');
+  })
 }
 
 addNavMenu ();
 
 function addPopupWindow() {
-    const button = document.querySelector('#header-popup-button');
-    const popupOverlay = document.querySelector('#popup-overlay');
-    button.addEventListener('click', function(event) {
-        addActiveClass(popupOverlay);
-    })
-    popupOverlay.addEventListener('click', function(event) {
-        if (event.target === popupOverlay) {
-            removeActiveClass(popupOverlay);
-        }
-    })
+  const button = document.querySelector('#header-popup-button');
+  const popupOverlay = document.querySelector('#popup-overlay');
+  button.addEventListener('click', function(event) {
+    addActiveClass(popupOverlay);
+  })
+  popupOverlay.addEventListener('click', function(event) {
+    if (event.target === popupOverlay) {
+      removeActiveClass(popupOverlay);
+    }
+  })
 }
 
 addPopupWindow();
 
-function TabElements (buttons, contentList) {
+class Tabs {
+  constructor(buttons, contentList) {
     this.buttons = buttons;
     this.contentList = contentList;
-    this.addTabs = function () {
-        this.buttons.forEach(function (button, index) {
-            button.addEventListener("click", function (event) {
-                switchTabs (
-                    button,
-                    index,
-                    buttons,
-                    contentList
-                );
-            });
-        });
-    }
+  }
+  addTabs() {
+    this.buttons.forEach((button, index) => {
+      button.addEventListener("click", () => {
+        switchTabs (
+          button,
+          index,
+          this.buttons,
+          this.contentList
+        );
+      });
+    });
+  }
 }
 
 function switchTabs (
-            button,
-            index,
-            buttons,
-            contentList,
-        ) {
+  button,
+  index,
+  buttons,
+  contentList
+) {
     const arrayOfTabLists = [
-        contentList,
-        buttons
+      contentList,
+      buttons
     ];
+
     arrayOfTabLists.forEach(function (array) {
-        array.forEach(removeActiveClass);
+      array.forEach(removeActiveClass);
     });
     
     const content = contentList[index];
     const arrayOfTabElements = [
-        content,
-        button
+      content,
+      button
     ];
     arrayOfTabElements.forEach(addActiveClass);
 }
 
-function removeActiveClass (element) {
-    element.classList.remove('active');
-}
-
-function addActiveClass (element) {
-    element.classList.add('active');
-}
-
 function linkTabs () {
-    const workstyleButtons = document.querySelectorAll('.workstyle__button');
-    const workstyleContentList = document.querySelectorAll('.workstyle__content');
-    const commandsButtons = document.querySelectorAll('.commands__button');
-    const commandsContentList = document.querySelectorAll('.commands__content');
-
-    const workstyleTabs = new TabElements (
-        workstyleButtons, workstyleContentList
-    );
-    const commandsTabs = new TabElements (
-        commandsButtons, commandsContentList
-    );
-    const arrayOfTabs = [
-        workstyleTabs,
-        commandsTabs
-    ]
-    arrayOfTabs.forEach(function (object) {
-        object.addTabs();
-    })
+  const workstyleButtons = document.querySelectorAll('.workstyle__button');
+  const workstyleContentList = document.querySelectorAll('.workstyle__content');
+  const commandsButtons = document.querySelectorAll('.commands__button');
+  const commandsContentList = document.querySelectorAll('.commands__content');
+  
+  const workstyleTabs = new Tabs (
+    workstyleButtons, 
+    workstyleContentList
+  );
+  const commandsTabs = new Tabs (
+    commandsButtons, 
+    commandsContentList
+  );
+  const arrayOfTabs = [
+      workstyleTabs,
+      commandsTabs
+  ]
+  arrayOfTabs.forEach(function (object) {
+      object.addTabs();
+  })
 }
 
 linkTabs ();
